26da59b487140b7c7afcac89cafc5330b277253b
杨月岗 <yangyg@cheche365.com>
2023-12-07 18:01:58 +0800

    [summary] sa-Token 添加自定义token生成规则

--- src/main/java/com/example/satoken/config/SaTokenConfigure.java
+++ src/main/java/com/example/satoken/config/SaTokenConfigure.java
@@ -1,16 +1,20 @@
 package com.example.satoken.config;
 
-import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.strategy.SaStrategy;
+import cn.dev33.satoken.util.SaFoxUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SaTokenConfigure implements WebMvcConfigurer {
-    // 注册 Sa-Token 拦截器，打开注解式鉴权功能 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // 注册 Sa-Token 拦截器，打开注解式鉴权功能 
-        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
+public class SaTokenConfigure {
+    /**
+     * 重写 Sa-Token 框架内部算法策略 
+     */
+    @Autowired
+    public void rewriteSaStrategy() {
+        // 重写 Token 生成策略 
+        SaStrategy.instance.createToken = (loginId, loginType) -> {
+            return SaFoxUtil.getRandomString(60);    // 随机60位长度字符串
+        };
     }
 }
