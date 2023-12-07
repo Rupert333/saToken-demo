85e92748c5cf218900de6b10875222fe385bb230
杨月岗 <yangyg@cheche365.com>
2023-12-07 17:15:58 +0800

    [summary] sa-Token 添加临时token生成方法，并指定tokenName

--- src/main/java/com/example/satoken/rest/LoginController.java
+++ src/main/java/com/example/satoken/rest/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.satoken.rest;
 
 import cn.dev33.satoken.annotation.SaCheckLogin;
+import cn.dev33.satoken.stp.SaTokenInfo;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,13 +20,29 @@ public class LoginController {
         return SaResult.ok("访问成功，此处为登录后才能看到的信息");
     }
 
-    // 登录接口  ---- http://localhost:8081/doLogin?name=zhang&pwd=123456
+    // 前后端一体模式的登录样例    ---- http://localhost:8081/NotCookie/doLogin?name=zhang&pwd=123456
     @RequestMapping("doLogin")
     public SaResult doLogin(String name, String pwd) {
-        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
         if("zhang".equals(name) && "123456".equals(pwd)) {
+            // 会话登录
             StpUtil.login(10001);
-            return SaResult.ok("登录成功");
+            return SaResult.ok();
+        }
+        return SaResult.error("登录失败");
+    }
+
+    // 前后端分离模式的登录样例    ---- http://localhost:8081/NotCookie/doLogin2?name=zhang&pwd=123456
+    @RequestMapping("doLogin2")
+    public SaResult doLogin2(String name, String pwd) {
+
+        if("zhang".equals(name) && "123456".equals(pwd)) {
+
+            // 会话登录
+            StpUtil.login(10001);
+
+            // 与常规登录不同点之处：这里需要把 Token 信息从响应体中返回到前端
+            SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+            return SaResult.data(tokenInfo);
         }
         return SaResult.error("登录失败");
     }
